50a8e290a338e8ab7bb83778877d6120714afbb9
SebuhiNurm <bd7zfwp0w@code.edu.az>
2024-03-27 20:03:46 +0400

    js

---
+++ 27.03.24/script.js
@@ -0,0 +1,47 @@
+// let verilen = window.prompt("daxil - et") ;
+
+// minutes = parseInt(verilen/60)
+// console.log(minutes); 
+// second = verilen - (minutes * 60)
+// console.log(second); 
+// console.log(minutes , "minutes ,", second, "second ,");
+
+// let temperatur = window.prompt("daxil - et") ;
+// selsi = 5 / 9 * (temperatur-32);
+// console.log(selsi);
+
+// let year = window.prompt("daxil - et") ;
+// age = 2024 - year;
+// console.log(age);
+
+// let a, b, c;
+// a = window.prompt("a katet daxil et") ;
+// b = window.prompt("b katet daxil et") ;
+// c = Math.sqrt(a**2 + b**2);
+// console.log(c);
+
+// let kredit = +window.prompt("kredit") ;
+// let faiz = +window.prompt("faiz") ;
+// let ay = +window.prompt("ay") ;
+// let netBorc = kredit + ((kredit * faiz) / 100);
+// let odenis = netBorc / ay ; 
+// console.log(netBorc)
+// console.log(odenis)
+
+
+// let ad = window.prompt("ad") ;
+// let soyad = window.prompt("soyad") ;
+// console.log(ad+" "+soyad+" salam")
+
+let a = +window.prompt("a") ;
+let perimetr = a*4;
+let sahe = a**2;
+console.log(perimetr+" perimetr"+sahe+" sahe")
+
+
+
+
+
+
+
+
